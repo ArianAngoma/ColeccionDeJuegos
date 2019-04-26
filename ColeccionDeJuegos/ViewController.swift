82a66edc8a0489fbbd0261dd7720ad7c2567d8c0
angoma850 <AngomaC15@macbook-air-de-mac.local>
2019-04-26 16:47:36 -0500

    Initial Commit

---
+++ ColeccionDeJuegos/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ColeccionDeJuegos
+//
+//  Created by Arian Angoma on 26/04/19.
+//  Copyright © 2019 angoma.pe.edu.tecsup. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
